9376e13305d427c1c2e97aaa36a6c14b0e78ca48
MannantaB <mannanta18@gmail.com>
2025-11-27 12:16:05 +0700

    add Entry destination route

---
+++ app/src/main/java/com/example/prak9/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.prak9.view.route
+
+import androidx.compose.ui.res.stringResource
+import com.example.prak9.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
